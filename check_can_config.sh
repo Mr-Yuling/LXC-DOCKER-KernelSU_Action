3cdd95293eacb72f0b4895a18beeb35cec18fb72
Mr-Yuling <waiting.fish.forever@gmail.com>
2024-06-14 20:03:16 +0800

    Add files via upload

---
+++ check_can_config.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+FILE=$1
+
+[ -f "$FILE" ] || {
+	echo "Provide a config file as argument"
+	
+	exit
+}
+
+write=false
+
+if [ "$2" = "-w" ]; then
+	write=true
+fi
+
+CONFIGS_ON="
+
+CONFIG_CAN
+CONFIG_CAN_RAW
+CONFIG_CAN_BCM
+CONFIG_CAN_GW
+CONFIG_CAN_J1939
+CONFIG_CAN_ISOTP
+CONFIG_CAN_DEV
+CONFIG_CAN_NETLINK
+CONFIG_CAN_CALC_BITTIMING
+CONFIG_CAN_8DEV_USB
+CONFIG_CAN_EMS_USB
+CONFIG_CAN_ESD_USB
+CONFIG_CAN_ETAS_ES58X
+CONFIG_CAN_GS_USB
+CONFIG_CAN_KVASER_USB
+CONFIG_CAN_MCBA_USB
+CONFIG_CAN_PEAK_USB
+CONFIG_CAN_UCAN
+
+"
+
+CONFIGS_OFF="
+"
+CONFIGS_EQ="
+"
+
+ered() {
+	echo -e "\033[31m" $@
+}
+
+egreen() {
+	echo -e "\033[32m" $@
+}
+
+ewhite() {
+	echo -e "\033[37m" $@
+}
+
+errors=0
+fixes=0
+
+for c in $CONFIGS_ON $CONFIGS_OFF;do
+	cnt=`grep -w -c $c $FILE`
+	if [ $cnt -gt 1 ];then
+		ered "$c appears more than once in the config file, fix this"
+		errors=$((errors+1))
+	fi
+
+	if [ $cnt -eq 0 ];then
+		if $write ; then
+			ewhite "Creating $c"
+			echo "# $c is not set" >> "$FILE"
+			fixes=$((fixes+1))
+		else
+			ered "$c is neither enabled nor disabled in the config file"
+			errors=$((errors+1))
+		fi
+	fi
+done
+
+for c in $CONFIGS_ON;do
+	if grep "$c=y\|$c=m" "$FILE" >/dev/null;then
+		egreen "$c is already set"
+	else
+		if $write ; then
+			ewhite "Setting $c"
+			sed  -i "s,# $c is not set,$c=y," "$FILE"
+			fixes=$((fixes+1))
+		else
+			ered "$c is not set, set it"
+			errors=$((errors+1))
+		fi
+	fi
+done
+
+for c in $CONFIGS_EQ;do
+	lhs=$(awk -F= '{ print $1 }' <(echo $c))
+	rhs=$(awk -F= '{ print $2 }' <(echo $c))
+	if grep "^$c" "$FILE" >/dev/null;then
+		egreen "$c is already set correctly."
+		continue
+	elif grep "^$lhs" "$FILE" >/dev/null;then
+		cur=$(awk -F= '{ print $2 }' <(grep "$lhs" "$FILE"))
+		ered "$lhs is set, but to $cur not $rhs."
+		if $write ; then
+			egreen "Setting $c correctly"
+			sed -i 's,^'"$lhs"'.*,# '"$lhs"' was '"$cur"'\n'"$c"',' "$FILE"
+			fixes=$((fixes+1))
+		fi
+	else
+		if $write ; then
+			ewhite "Setting $c"
+			echo  "$c" >> "$FILE"
+			fixes=$((fixes+1))
+		else
+			ered "$c is not set"
+			errors=$((errors+1))
+		fi
+	fi
+done
+
+for c in $CONFIGS_OFF;do
+	if grep "$c=y\|$c=m" "$FILE" >/dev/null;then
+		if $write ; then
+			ewhite "Unsetting $c"
+			sed  -i "s,$c=.*,# $c is not set," $FILE
+			fixes=$((fixes+1))
+		else
+			ered "$c is set, unset it"
+			errors=$((errors+1))
+		fi
+	else
+		egreen "$c is already unset"
+	fi
+done
+
+if [ $errors -eq 0 ];then
+	egreen "\n\nConfig file checked, found no errors.\n\n"
+else
+	ered "\n\nConfig file checked, found $errors errors that I did not fix.\n\n"
+fi
+
+if [ $fixes -gt 0 ];then
+	egreen "开启配置 $fixes 项.\n\n"
+fi
+
+ewhite " "
